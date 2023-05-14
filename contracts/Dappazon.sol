0ffd4fedb00632b318bd132d08a196d2d7657444
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2023-05-14 18:53:09 +0700

    updarte contract and test

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -21,6 +21,13 @@ contract Dappazon {
     // mapping of product id to product
     mapping(uint256 => Product) public products;
 
+    // events ListProduct
+    event ListProduct(
+        string name,
+        uint256 cost,
+        uint256 quantity
+    );
+
     // q: what is constructor?
     // a: constructor is a function that is called when the contract is created
     constructor() {
@@ -28,6 +35,12 @@ contract Dappazon {
 
         // set the owner of the contract
         owner = msg.sender;
+    }
+
+    // modifier to check if the caller is the owner of the contract
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
     }
 
     // add product to the Blockchain
@@ -39,7 +52,13 @@ contract Dappazon {
         uint256 _cost,
         uint256 _rating,
         uint256 _stock
-    ) public {
+    ) public  {
+        // check if the product with the given id already exists
+        require(
+            products[_id].id != _id,
+            "Product with the given id already exists"
+        );
+
         // create a new product
         Product memory newProduct = Product(
             _id,
@@ -53,5 +72,33 @@ contract Dappazon {
 
         // add the product to the mapping of products
         products[_id] = newProduct;
+
+        // emit the event ListProduct
+        emit ListProduct(_name, _cost, _stock);
+    }
+
+    // buy product from the Blockchain
+    function buyProduct(uint256 _id, uint256 _quantity) public payable {
+        // check if the product with the given id exists
+        require(
+            products[_id].id == _id,
+            "Product with the given id does not exist"
+        );
+
+        // check if the product is in stock
+        require(
+            products[_id].stock >= _quantity,
+            "Product is out of stock"
+        );
+
+        // check if the caller has enough ether to buy the product
+        require(
+            msg.sender.balance >= products[_id].cost * _quantity,
+           
+            "Caller does not have enough ether to buy the product"
+        );
+
+        // reduce the stock of the product
+        products[_id].stock -= _quantity;
     }
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,8 +1,18 @@
 const { expect } = require("chai");
 
+// q: what is tokens do?
+
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), "ether");
+  return ethers.utils.parseUnits(n.toString(), "ether"); // 
 };
+
+const id = 1
+const name= "iPhone 12"    // name
+const category=        "Mobile Phone" // category
+const image ="https://gateway.ipfs.io/ipfs/QmPPFpFhdJSB4T6pn3vKinsDMgJeiqQ3JTDoLarvBsug3w/product-5.jpg"          // image      
+const cost = tokens(1)              // cost
+const rating = 4              // rating
+const stock = 5               // stock
 
 describe("Dappazon", () => {
   // declare variables
@@ -19,7 +29,9 @@ describe("Dappazon", () => {
     console.log("owner address: ", owner.address);
     console.log("owner value: ", ethers.utils.formatEther(await owner.getBalance()));
     console.log("buyer address: ", buyer.address);
+    console.log("buyer value: ", ethers.utils.formatEther(await buyer.getBalance()));
     console.log("seller address: ", seller.address);
+    console.log("seller value: ", ethers.utils.formatEther(await seller.getBalance())); 
 
     // get the contract factory
     const Dappazon = await ethers.getContractFactory("Dappazon");
@@ -45,7 +57,7 @@ describe("Dappazon", () => {
 
   });
 
-  describe("Products", async () => {
+  describe("Add Products", async () => {
     // list of products
     let result
     let productCount
@@ -55,14 +67,14 @@ describe("Dappazon", () => {
       let product
 
       // add products
-      product = await dappazon.connect(owner).addProduct(
-        1,              // id
-        "iPhone 12",    // name
-        "Mobile Phone", // category
-        "url",          // image      
-        1,              // cost
-        4,              // rating
-        5               // stock
+      product = await dappazon.connect(seller).addProduct(
+        id,
+        name,
+        category,
+        image,         // image      
+        cost,              // cost
+        rating,              // rating
+        stock               // stock
       )
 
       // add products to the list of products and wait for it to be mined
@@ -71,23 +83,61 @@ describe("Dappazon", () => {
     });
 
     // test case 1 - check if the product was added
-    it("Should add a product", async () => {
-      const product = await dappazon.products(1);
-      expect(product.id).to.equal(1);
-      expect(product.name).to.equal("iPhone 12");
-      expect(product.category).to.equal("Mobile Phone");
-      expect(product.image).to.equal("url");
-      expect(product.cost).to.equal(1);
-      expect(product.rating).to.equal(4);
-      expect(product.stock).to.equal(5);
+    it("Should list the added a product correctly", async () => {
+      const product = await dappazon.products(1);   // get the product
 
-      
+      expect(product.id).to.equal(id);
+      expect(product.name).to.equal(name);
+      expect(product.category).to.equal(category);
+      expect(product.image).to.equal(image);
+      expect(product.cost).to.equal(cost);
+      expect(product.rating).to.equal(rating);
+      expect(product.stock).to.equal(stock);
+    });
+
+    // test case 2 - check if the product was listed correctly by event emitted
+    it("Should list a product with event emitted", async () => {
+      const product = await dappazon.products(1);   // get the product
+      expect(product).to.emit(dappazon,"ListProduct")
+    });
+  });
+
+  describe("Buy Products", async () => {
+    // list of products
+    let result
+    let productCount
+
+    // run before each test
+    beforeEach(async () => {
+      let product
+
+      // add products
+      product = await dappazon.connect(seller).addProduct(
+        id,
+        name,
+        category,
+        image,         // image      
+        cost,              // cost
+        rating,              // rating
+        stock               // stock
+      )
+
+      // add products to the list of products and wait for it to be mined
+      await product.wait()
 
     });
 
+    // test case 1 - check if the product was bought correctly
+    it("Should buy a product correctly", async () => {
+
+      // buy the product
+      result = await dappazon.connect(buyer).buyProduct(1, 1);
+
+      const product = await dappazon.products(1);   // get the product
+
+      // check if the product was bought correctly
+      expect(product.stock).to.equal(4);
+    });
     
-
   });
-
-
 });
